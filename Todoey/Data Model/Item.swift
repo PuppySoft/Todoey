a8683f2f11291f22be9ebc5676aceb468dfb3150
PuppySoft <daniel.tyrode@outlook.com>
2018-03-05 11:08:57 +0000

    Convert to MVC architecture

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,17 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Daniel on 05/03/2018.
+//  Copyright © 2018 Daniel Tyrode. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    
+    var title : String = ""
+    var done : Bool = false
+    
+    
+}
